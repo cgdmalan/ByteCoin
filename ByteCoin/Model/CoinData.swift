d808cdf556118c5eb12be9852ead963a9c0a8e3c
Cigdem Alan <cigdem.alan@outlook.com>
2023-12-29 15:46:36 +0300

    main

---
+++ ByteCoin/Model/CoinData.swift
@@ -0,0 +1,18 @@
+//
+//  CoinData.swift
+//  ByteCoin
+//
+//  Created by Çiğdem Alan on 5.12.2023.
+//  Copyright © 2023 The App Brewery. All rights reserved.
+//
+
+import Foundation
+
+
+struct CoinData : Decodable {
+    
+    let rate : Double
+   
+   
+}
+
